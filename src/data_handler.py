08ba48015172ccfe0c4f6a0bda1a264ebafcc2ba
a-buch <anna.buch@tu-berlin.de>
2026-03-04 10:55:22 +0100

    feat: init handling of misisng values

--- src/data_handler.py
+++ src/data_handler.py
@@ -7,8 +7,20 @@ __email__ = "anna.buch@tu-berlin.de"
 
 from pathlib import Path
 from glob import glob
+import numpy as np
+import pandas as pd
 
 
-def load_text_sources(data_dir):
-    text_sources = glob(str(Path(data_dir, "*_cleaned.md" )))
+
+def load_text_sources(data_dir: Path) -> list:
+
+    text_sources = glob(str(Path(data_dir, "*_cleaned.md")))
+    
     return text_sources
+
+
+def replace_missing_values(df:pd.DataFrame) -> pd.DataFrame:
+    df = df.replace(
+        {-999: np.nan, "-999": np.nan, None: np.nan}
+    )
+    return df
