6d99bce036512547ddb1c1e1e11655ac190b0113
a-buch <anna.buch@tu-berlin.de>
2026-03-04 11:06:20 +0100

    feat: for preprocessing

--- src/data_handler.py
+++ src/data_handler.py
@@ -8,10 +8,10 @@ __email__ = "anna.buch@tu-berlin.de"
 from pathlib import Path
 from glob import glob
 
+
 def load_text_sources(data_dir):
-    
     text_sources = glob(str(Path(data_dir, ["*_cleaned.md", "*_cleaned.txt"])))
-    
+
     return text_sources
 
 
